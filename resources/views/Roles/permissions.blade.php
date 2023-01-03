7657d05127dc59bad9f861c7baf6eb1b8f0801e0
sifoune aissa <aissa23sifoune@gmail.com>
2023-01-03 02:14:12 +0100

    # permissions blade

--- resources/views/Roles/permissions.blade.php
+++ resources/views/Roles/permissions.blade.php
@@ -1,5 +1,6 @@
 @foreach($model->getAllPermissions()->pluck('name') as $permission)
     @if($loop->index<=10)
-    <span class="badge badge-primary">{{$permission}}</span>
+    <span class="badge badge-primary">{{$permission}}</span>@if($loop->index==10)<span class="h5"> ......</span>@endif
     @endif
+
 @endforeach
