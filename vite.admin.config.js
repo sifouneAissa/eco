70b741dc4449a0713e52ff6b2937d5ffbf6bf0d9
sifoune aissa <aissa23sifoune@gmail.com>
2023-01-07 05:25:46 +0100

    hot admin,client in production

---
+++ vite.admin.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app-admin.js',
+            ],
+            hotFile: 'public/admin.hot',
+            buildDirectory: 'build-admin',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+
+    ],
+});
