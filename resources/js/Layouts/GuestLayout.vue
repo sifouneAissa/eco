47d22d582caed4833c18d2b7065f69ed76af7938
sifoune aissa <aissa23sifoune@gmail.com>
2023-01-16 01:56:41 +0100

    add to cart

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -15,6 +15,8 @@
     import {useToast} from "vue-toastification";
     import {usePage} from "@inertiajs/inertia-vue3";
     import CartMenu from "@/Pages/Checkout/CartMenu.vue"
+    import QuickViewModal from "@/Pages/Modals/QuickViewModal.vue"
+    import AddToCartModal from "@/Pages/Modals/AddToCartModal.vue"
 
     export default {
         components: {
@@ -29,7 +31,9 @@
             ApplicationMark,
             Link,
             Head,
-            CartMenu
+            CartMenu,
+            QuickViewModal,
+            AddToCartModal
         },
         mounted() {
             // clear interval
@@ -55,22 +59,11 @@
 
             let app = this;
 
-            let add_to_cart_modal = $("#add_to_cart_modal");
-            let quick_view_modal = $("#quick_view_modal");
-
-
-            add_to_cart_modal.on("hidden.bs.modal", function () {
-                this.$page.props.product=null;
-            });
-            quick_view_modal.on("hidden.bs.modal", function () {
-                this.$page.props.product=null;
-            });
-
-
             $("#script1").remove();
             $("#script2").remove();
 
             $(document).ready(function () {
+
                 if (app.$page.props.isRtl) {
                     // for lazy importation
                     let script1 = document.createElement('script');
@@ -103,6 +96,8 @@
                     script2.async = true;
                     document.body.append(script2); // (*)
                 }
+
+
 
 
             });
@@ -867,169 +862,8 @@
             </footer>
             <!-- FOOTER AREA END -->
 
-            <!-- MODAL AREA START (Quick View Modal) -->
-            <div v-if="$page.props.product" class="ltn__modal-area ltn__quick-view-modal-area">
-                <div :key="$page.props.product.id" class="modal fade" id="quick_view_modal" tabindex="-1">
-                    <div class="modal-dialog modal-lg" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    <!-- <i class="fas fa-times"></i> -->
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="ltn__quick-view-modal-inner">
-                                    <div class="modal-product-item">
-                                        <div class="row">
-                                            <div class="col-lg-6 col-12">
-                                                <div class="modal-product-img">
-                                                    <img :src="$page.props.product.fimage" alt="#">
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6 col-12">
-                                                <div class="modal-product-info">
-                                                    <div class="product-ratting">
-                                                        <ul>
-                                                            <li><a href="#"><i class="fas fa-star"></i></a></li>
-                                                            <li><a href="#"><i class="fas fa-star"></i></a></li>
-                                                            <li><a href="#"><i class="fas fa-star"></i></a></li>
-                                                            <li><a href="#"><i class="fas fa-star-half-alt"></i></a>
-                                                            </li>
-                                                            <li><a href="#"><i class="far fa-star"></i></a></li>
-                                                            <li class="review-total"><a href="#"> ( 95 Reviews )</a>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <h3>{{$page.props.product.name}}</h3>
-                                                    <div class="product-price">
-                                                        <span>{{ $page.props.currency_code }} {{ $page.props.product.cprice }}</span>
-                                                        <del>$165.00</del>
-                                                    </div>
-                                                    <div class="modal-product-meta ltn__product-details-menu-1">
-                                                        <ul>
-                                                            <li>
-                                                                <strong>Category : </strong>
-                                                                <span>
-                                                                 <a href="#">{{$page.props.product.category.name}}</a>
-<!--                                                            <a href="#">Oil</a>-->
-<!--                                                            <a href="#">Grooming</a>-->
-<!--                                                            <a href="#">Tools</a>-->
-                                                        </span>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="ltn__product-details-menu-2">
-                                                        <ul>
-                                                            <li>
-                                                                <div class="cart-plus-minus">
-                                                                    <input type="text" value="02" name="qtybutton"
-                                                                           class="cart-plus-minus-box">
-                                                                </div>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#" class="theme-btn-1 btn btn-effect-1"
-                                                                   title="Add to Cart" data-toggle="modal"
-                                                                   data-target="#add_to_cart_modal">
-                                                                    <i class="fas fa-shopping-cart"></i>
-                                                                    <span>ADD TO CART</span>
-                                                                </a>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <div class="ltn__product-details-menu-3">
-                                                        <ul>
-                                                            <li>
-                                                                <a href="#" class="" title="Wishlist"
-                                                                   data-toggle="modal"
-                                                                   data-target="#liton_wishlist_modal">
-                                                                    <i class="far fa-heart"></i>
-                                                                    <span>Add to Wishlist</span>
-                                                                </a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#" class="" title="Compare" data-toggle="modal"
-                                                                   data-target="#quick_view_modal">
-                                                                    <i class="fas fa-exchange-alt"></i>
-                                                                    <span>Compare</span>
-                                                                </a>
-                                                            </li>
-                                                        </ul>
-                                                    </div>
-                                                    <hr>
-                                                    <div class="ltn__social-media">
-                                                        <ul>
-                                                            <li>Share:</li>
-                                                            <li><a href="#" title="Facebook"><i
-                                                                class="fab fa-facebook-f"></i></a></li>
-                                                            <li><a href="#" title="Twitter"><i
-                                                                class="fab fa-twitter"></i></a></li>
-                                                            <li><a href="#" title="Linkedin"><i
-                                                                class="fab fa-linkedin"></i></a></li>
-                                                            <li><a href="#" title="Instagram"><i
-                                                                class="fab fa-instagram"></i></a></li>
-
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- MODAL AREA END -->
-
-            <!-- MODAL AREA START (Add To Cart Modal) -->
-            <div  v-if="$page.props.product" class="ltn__modal-area ltn__add-to-cart-modal-area">
-                <div :key="$page.props.product.id" class="modal fade" id="add_to_cart_modal" tabindex="-1">
-                    <div class="modal-dialog modal-md" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="ltn__quick-view-modal-inner">
-                                    <div class="modal-product-item">
-                                        <div class="row">
-                                            <div class="col-12">
-                                                <div class="modal-product-img">
-                                                    <img :src="$page.props.product.fimage" alt="#">
-                                                </div>
-                                                <div class="modal-product-info">
-                                                    <h5><a href="product-details.html">{{$page.props.product.name}}</a></h5>
-                                                    <p class="added-cart"><i class="fa fa-check-circle"></i>
-                                                        Successfully added to your Cart</p>
-                                                    <div class="btn-wrapper">
-                                                        <a href="cart.html" class="theme-btn-1 btn btn-effect-1">View
-                                                            Cart</a>
-                                                        <a href="checkout.html" class="theme-btn-2 btn btn-effect-2">Checkout</a>
-                                                    </div>
-                                                </div>
-                                                <!-- additional-info -->
-                                                <div class="additional-info d-none">
-                                                    <p>We want to give you <b>10% discount</b> for your first order,
-                                                        <br> Use discount code at checkout</p>
-                                                    <div class="payment-method">
-                                                        <img src="img/icons/payment.png" alt="#">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- MODAL AREA END -->
-
+            <AddToCartModal/>
+            <QuickViewModal />
             <!-- MODAL AREA START (Wishlist Modal) -->
             <div class="ltn__modal-area ltn__add-to-cart-modal-area">
                 <div class="modal fade" id="liton_wishlist_modal" tabindex="-1">
---
+++ resources/js/Pages/Modals/AddToCartModal.vue
@@ -0,0 +1,134 @@
+<template>
+
+    <!-- MODAL AREA START (Add To Cart Modal) -->
+    <div   class="ltn__modal-area ltn__add-to-cart-modal-area">
+        <div class="modal fade" id="add_to_cart_modal" tabindex="-1">
+            <div v-if="$page.props.product" :key="$page.props.product.id"  class="modal-dialog modal-md" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+
+                    </div>
+                    <div class="modal-body">
+                        <div class="ltn__quick-view-modal-inner">
+                            <div class="modal-product-item">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="modal-product-img">
+                                            <img :src="$page.props.product.fimage" alt="#">
+                                        </div>
+                                        <div class="modal-product-info">
+                                            <h5><a href="product-details.html">{{$page.props.product.name}}</a></h5>
+
+                                            <div class="row">
+                                                <span
+                                                    v-if="form.processing"
+                                                    class="spinner-grow spinner-grow-md"
+                                                    role="status"
+                                                    aria-hidden="true"
+                                                ></span>
+                                                <p  class="added-cart">
+                                                    <i v-if="!form.processing" class="fa fa-check-circle"></i>
+                                                    Successfully added to your Cart</p>
+                                            </div>
+                                            <div class="btn-wrapper">
+                                                <a href="cart.html" class="theme-btn-1 btn btn-effect-1">View
+                                                    Cart</a>
+                                                <a href="checkout.html" class="theme-btn-2 btn btn-effect-2">Checkout</a>
+                                            </div>
+                                        </div>
+                                        <!-- additional-info -->
+                                        <div class="additional-info d-none">
+                                            <p>We want to give you <b>10% discount</b> for your first order,
+                                                <br> Use discount code at checkout</p>
+                                            <div class="payment-method">
+                                                <img src="img/icons/payment.png" alt="#">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- MODAL AREA END -->
+
+</template>
+<script>
+
+    import {useToast} from "vue-toastification";
+    import {useForm} from "@inertiajs/inertia-vue3";
+    import {Inertia} from "@inertiajs/inertia";
+
+    export default  {
+
+        data () {
+            return {
+                form : useForm({
+                })
+            }
+        },
+        mounted() {
+            let app = this;
+            let add_to_cart_modal = $("#add_to_cart_modal");
+
+            add_to_cart_modal.on("hidden.bs.modal", function () {
+                app.$page.props.product=null;
+            });
+            //
+            //
+            add_to_cart_modal.on("show.bs.modal", function (e) {
+                app.submit(app.$page.props.product);
+            });
+        },
+        methods: {
+            incrQP(item, by) {
+                const toast = useToast();
+                // let q = item.quantity;
+                let product = item;
+                product.quantity = product.quantity + by;
+
+                if (item.quantity > product.isA.remain) {
+                    item.quantity = product.isA.remain;
+                    toast.warning(this.$t("listing.limited")  + product.name);
+                }
+
+                if (!item.quantity) item.quantity = 1;
+
+                // if (q !== item.quantity) this.submit(item, by);
+            },
+            submit: function (model) {
+                let app = this;
+                this.form
+                    .transform((data) => ({
+                        product_id: model.id,
+                        quantity: 1,
+                    }))
+                    .post(route("addProduct"),{
+                        onSuccess : function (){
+                            app.$page.props.product = model;
+                        }
+                    });
+            },
+            submitD(item) {
+                Inertia.delete(
+                    route("cartitem.destroy", {
+                        id: item.id,
+                    }),
+                    {
+                        onSuccess: (res) => {},
+                    }
+                );
+            },
+            Pay() {
+                this.$emit("Pay");
+            },
+        },
+    }
+
+</script>
