1adc0ae3d147f99bd2a01ad31dc3120c3e7c1421
sifoune aissa <aissa23sifoune@gmail.com>
2023-01-17 20:45:55 +0100

    # add files

---
+++ app/Http/Controllers/WishListController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wishlist;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class WishListController extends Controller
+{
+    //
+
+
+    public function index(){
+
+        $models = Wishlist::where('user_id',auth()->user()->id)->with(['user','product.category'])->orderBy('created_at','desc')->get();
+
+        return Inertia::render('WishList',[
+            'models' => $models
+        ]);
+    }
+
+    public function store(Request $request){
+
+        Wishlist::create(array_merge(
+            filterRequest($request->all(),Wishlist::class),
+            [
+                'user_id' => auth()->user()->id
+            ]
+        ));
+    }
+
+    public function destroy($id){
+        Wishlist::find($id)?->delete();
+    }
+}
