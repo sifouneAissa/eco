88f3e1ee0ca2a14aa011c23d7e7eee645b755ed8
sifoune aissa <aissa23sifoune@gmail.com>
2023-01-20 22:15:05 +0100

    Product card width

--- resources/js/Pages/Listing/ProductCard.vue
+++ resources/js/Pages/Listing/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ltn__product-item ltn__product-item-3 text-center">
         <div class="product-img">
-            <a @click="go()" href="javascript: void(0)"><img style="height: 200px" :src="model.fimage" alt="#"></a>
+            <a @click="go()" href="javascript: void(0)"><img style="height: 200px;width: 100%" :src="model.fimage" alt="#"></a>
 <!--            <div class="product-badge ">-->
 <!--                <ul >-->
 <!--                    <li v-if="model.popular" class="sale-badge">Popular</li>-->
