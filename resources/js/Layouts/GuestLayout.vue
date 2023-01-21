4d9f5cb9abda2cf2089105a08538c4c528ded32c
unknown <wissam.kechkar@gmail.com>
2023-01-21 15:01:46 +0100

    nav menu

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -297,17 +297,44 @@ export default {
                             </li>
                             <!-- <li><a href="coming-soon.html">Coming Soon</a></li> -->
                             <!-- <li><a href="404.html">404</a></li> -->
-                            <li>
+                            <!-- <li>
                               <Link :href="route('contact-us')">{{
                                 $t("nav_menu.help.contact_us")
                               }}</Link>
-                            </li>
+                            </li> -->
                           </ul>
                         </li>
                         <li>
-                          <Link :href="route('listing')"
-                            >{{ $t("nav_menu.pages.products") }}
-                          </Link>
+                          <a href="#">{{ $t("nav_menu.pages.products") }} </a>
+                          <ul>
+                            <li>
+                              <Link :href="route('listing')"
+                                >{{ $t("nav_menu.pages.products") }}
+                              </Link>
+                            </li>
+                            <li>
+                              <a href="#"
+                                >Other Pages <span class="float-right">>></span></a
+                              >
+                              <ul>
+                                <li>
+                                  <Link
+                                    v-if="$page.props.auth"
+                                    :href="route('wishlist.index')"
+                                    >Wishlist</Link
+                                  >
+                                </li>
+                                <li>
+                                  <Link :href="route('checkout.show')">Checkout</Link>
+                                </li>
+                                <li>
+                                  <Link :href="route('trackOrder.index')"
+                                    >Order Tracking</Link
+                                  >
+                                </li>
+                              </ul>
+                            </li>
+                          </ul>
                         </li>
                         <!-- <li class="menu-icon">
                           <a href="#">Kits</a>
@@ -700,7 +727,11 @@ export default {
                   <li><a href="shop-grid.html">Shop Grid</a></li>
                   <li><a href="product-details.html">Shop details </a></li> -->
                   <li><Link :href="route('cartitem.index')">Cart</Link></li>
-                  <li><Link v-if="$page.props.auth" :href="route('wishlist.index')">Wishlist</Link></li>
+                  <li>
+                    <Link v-if="$page.props.auth" :href="route('wishlist.index')"
+                      >Wishlist</Link
+                    >
+                  </li>
                   <li><Link :href="route('checkout.show')">Checkout</Link></li>
                   <li><Link :href="route('trackOrder.index')">Order Tracking</Link></li>
                   <li>
@@ -708,14 +739,14 @@ export default {
                       {{ $t("nav_menu.pages.account") }}</Link
                     >
                   </li>
-<!--                  <li>-->
-<!--                    <Link :href="route('login')"> {{ $t("nav_menu.pages.login") }}</Link>-->
-<!--                  </li>-->
-<!--                  <li>-->
-<!--                    <Link :href="route('register')"-->
-<!--                      >{{ $t("nav_menu.pages.register") }}-->
-<!--                    </Link>-->
-<!--                  </li>-->
+                  <!--                  <li>-->
+                  <!--                    <Link :href="route('login')"> {{ $t("nav_menu.pages.login") }}</Link>-->
+                  <!--                  </li>-->
+                  <!--                  <li>-->
+                  <!--                    <Link :href="route('register')"-->
+                  <!--                      >{{ $t("nav_menu.pages.register") }}-->
+                  <!--                    </Link>-->
+                  <!--                  </li>-->
                 </ul>
               </li>
               <li>
@@ -958,8 +989,14 @@ export default {
                   <h4 class="footer-title">{{ $t("footer.services") }}</h4>
                   <div class="footer-menu">
                     <ul>
-                      <li><a href="#">Order tracking</a></li>
-                      <li><a href="#">Wish List</a></li>
+                      <li>
+                        <Link :href="route('trackOrder.index')">Order Tracking</Link>
+                      </li>
+                      <li>
+                        <Link v-if="$page.props.auth" :href="route('wishlist.index')"
+                          >Wish List</Link
+                        >
+                      </li>
                       <li>
                         <Link :href="route('login')">
                           {{ $t("nav_menu.pages.login") }}</Link
@@ -997,8 +1034,14 @@ export default {
                           {{ $t("nav_menu.pages.account") }}</Link
                         >
                       </li>
-                      <li><a href="#">Wish List</a></li>
-                      <li><a href="#">Order tracking</a></li>
+                      <li>
+                        <Link v-if="$page.props.auth" :href="route('wishlist.index')"
+                          >Wish List</Link
+                        >
+                      </li>
+                      <li>
+                        <Link :href="route('trackOrder.index')">Order Tracking</Link>
+                      </li>
                       <li>
                         <Link :href="route('faq')">
                           {{ $t("nav_menu.help.faq") }}
